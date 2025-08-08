04fbf265fec933b1237e9126155000f2d21b3505
chdan-hub <ksmii358@gmail.com>
2025-08-08 00:37:01 -0700

    1일차 강의

---
+++ 1일차_실습/04.route/app.py
@@ -0,0 +1,46 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello, This is my page!"
+
+@app.route('/about')
+def about():
+    return "This is about page!"
+
+@app.route('/user/<username>')
+def user_profile(username):
+    return f'UserName : {username}'
+
+@app.route('/number/<int:number>')
+def number(number):
+    return f'number : {number}'
+
+# post 요청 날리는 법
+# (1) postman
+# (2) requests
+import requests 
+@app.route('/test')
+def test():
+    url = 'http://127.0.0.1:5000/submit'
+    data = 'test data'
+    response = requests.post(url=url, data=data)
+
+    return response
+
+@app.route('/submit', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def submit():
+    print(request.method)
+
+    if request.method == 'GET':
+        print("GET method")
+
+    if request.method == 'POST':
+        print('***POST method***', request.data)
+
+    return Response("Sucessfully submitted", status=200)
+
+if __name__ == "__main__":
+    app.run()
